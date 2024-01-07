d1b7b840ea02bfa2bd1b73176361e2785edb5d6d
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-07 09:19:11 -0600

    Update menu.h

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -7,11 +7,55 @@
 
 using namespace ImGui;
 
+
+void styl() {
+	ImGuiStyle& style = ImGui::GetStyle();
+
+    style.Colors[ImGuiCol_Text] = ImColor(255, 255, 255);
+    style.Colors[ImGuiCol_WindowBg] = ImColor(0, 2, 13);
+    style.Colors[ImGuiCol_TitleBg] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_TitleBgActive] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_Button] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ButtonActive] = ImColor(130, 0, 177);
+    style.Colors[ImGuiCol_ButtonHovered] = ImColor(130, 0, 177);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ScrollbarGrab] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImColor(111, 123, 247);
+    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImColor(130,0,177);
+    style.Colors[ImGuiCol_ResizeGripActive] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ResizeGrip] = ImColor(13, 118, 220);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_TitleBgActive] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_CheckMark] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_SliderGrab] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_HeaderActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_HeaderHovered] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_Header] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_FrameBg] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_FrameBgActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_Button] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_ButtonHovered] = ImColor(150,0,205);
+	style.Colors[ImGuiCol_ButtonActive] = ImColor(150,0,205);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImColor(0, 2, 13);
+
+	style.Colors[ImGuiCol_PopupBg] = ImColor(0, 2, 13);
+	
+
+    style.WindowTitleAlign = ImVec2(0.5, 0.5);
+    style.FramePadding = ImVec2(10, 10);
+    style.FrameRounding = 2.5;
+    style.WindowRounding = 5.f;
+    style.WindowBorderSize = 4.f;
+    }
+
+
 void DrawMenu()
-{
+{ styl();
     static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     {
-        Begin(OBFUSCATE("ZyCheats"));
+        Begin(OBFUSCATE("Test Nigga"));
         ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_FittingPolicyResizeDown;
         if (BeginTabBar("Menu", tab_bar_flags)) {
                 
