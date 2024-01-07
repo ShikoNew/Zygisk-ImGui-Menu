b5a3dce8a437f9258c572551d14974b41622a9ce
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-07 09:53:27 -0600

    Update functions.h

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -80,6 +80,7 @@ void* ProductDefinition(void *instance, monoString* id, monoString* storeSpecifi
 }
 
 void Hooks() {
+    HOOK("0x12cdfd0", isGodMode, old_ApplyDamagePlayer);
     HOOK("0xE7BC74", Backend, old_Backend);
     HOOK("0x29DA08C", ProductDefinition, old_ProductDefinition);
 }
