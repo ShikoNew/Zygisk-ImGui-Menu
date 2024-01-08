52513ed93425d9dab5a402c66f1c5c2334efd031
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-07 22:40:10 -0600

    Update menu.h

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -68,7 +68,7 @@ void DrawMenu()
                 Checkbox(OBFUSCATE("No kick"), &nokick);
                 TextUnformatted(OBFUSCATE("Damage Multiplier:")); // just plain text
 
-                SliderInt(OBFUSCATE("Speed"), &damageMultiplie, 1, 100, OBFUSCATE("%d"), 0); // min = 1, max = 100
+                SliderInt(OBFUSCATE("Speed"), &damageMultiplie, 1, 1000, OBFUSCATE("%d"), 0); // min = 1, max = 100
                 Checkbox(OBFUSCATE("God Mode"), &isGod);
                 // here menu stuff, remove test btw
                 // ImGui::Checkbox(OBFUSCATE("This is a checkbox"), &test);
