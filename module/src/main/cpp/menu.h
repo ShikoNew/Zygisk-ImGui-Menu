1b4bd8f68bcb8e2bd6599794969cbb52a68940f3
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-08 11:56:15 -0600

    Update menu.h

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -14,49 +14,6 @@ void DrawMenu()
                 Checkbox(OBFUSCATE("Test"), &test);
                 EndTabItem();
             }
-             if (BeginTabItem(OBFUSCATE("Player"))) {
-///
-
-
-Checkbox(OBFUSCATE("anti close"), &bypass);
-                Checkbox(OBFUSCATE("No kick"), &nokick);
-                TextUnformatted(OBFUSCATE("Damage Multiplier:")); // just plain text
-
-                SliderInt(OBFUSCATE("Speed"), &damageMultiplie, 1, 1000, OBFUSCATE("%d"), 0); // min = 1, max = 100
-                Checkbox(OBFUSCATE("God Mode"), &isGod);
-                // here menu stuff, remove test btw
-                // ImGui::Checkbox(OBFUSCATE("This is a checkbox"), &test);
-                if (Button(OBFUSCATE("Add Currency"))) {
-                    // code for button action
-                    addCurrency = true;
-                }
-                TextUnformatted(OBFUSCATE("Adds 1000 gems"));
-                if (Button(OBFUSCATE("Add Skins"))) {
-                    // code for button action
-                    addSkins = true;
-                }
-                Checkbox(OBFUSCATE("Everything unlocked"), &everythingUnlocked);
-                Checkbox(OBFUSCATE("Free Items"), &freeItems);
-                Checkbox(OBFUSCATE("Show Items"), &showAllItems);
-
-
-///end
-                EndTabItem();
-            }
-            
-if (BeginTabItem(OBFUSCATE("Player2"))) {
-	SliderFloat(OBFUSCATE("Jump Higher"), &jumpfloat, 0, 0.05f);
-                TextUnformatted(OBFUSCATE("Damage Multiplier:")); // just plain text
-
-                //SliderInt(OBFUSCATE("Damage Multiplier"), &damageMultiplie, 1, 100, OBFUSCATE("%d"), 0); // min = 1, max = 100
-             //   Checkbox(OBFUSCATE("God Mode"), &isGod);
-                // here menu stuff, remove test btw
-                // ImGui::Checkbox(OBFUSCATE("This is a checkbox"), &test);
-               
-                EndTabItem();
-            }
-
-            
             EndTabBar();
         }
         Pointers();
