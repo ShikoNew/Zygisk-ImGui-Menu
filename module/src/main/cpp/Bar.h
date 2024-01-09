72143ec979085e427189e4218834fe5c174bf2a2
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-09 13:20:55 -0600

    Create Bar.h

---
+++ module/src/main/cpp/Bar.h
@@ -0,0 +1,5 @@
+static int m16rn = 0;
+const char* m16type[] = { "Disable", "Blue","Blue fantasy", "XD?", "Winged", "Needle", "Muraena", "Facet", "Camouflage", "Retro Arcade", "Sharps Shooter"};
+static int sizer = 0;
+const char* sizetype[] = { "Disable", "Blue","Blue fantasy", "XD?", "Winged", "Needle", "Muraena", "Facet", "Camouflage", "Retro Arcade", "Sharps Shooter"};
+
