2bc6c28c8a27c425c64913f0cda3d2a7005e600a
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-08 18:36:43 -0600

    Update menu.h

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -96,7 +96,7 @@ page = 1;
 page = 2;
     if(ImGui::CollapsingHeader("Speed"))
 	
-    {SliderFloat(OBFUSCATE("Speed"), &jumpfloat, 0.1, 0.5);} // min = 1, max = 100}
+    {Checkbox(OBFUSCATE("God Mode"), &Gud);} // min = 1, max = 100}
 	if(ImGui::CollapsingHeader("Sandbox"))
 			{}
     } else if (page == 3) {
