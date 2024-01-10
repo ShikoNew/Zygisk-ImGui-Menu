112d0d6a44af87632582ee78127c3a9c41c02323
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-09 23:51:02 -0600

    Create Custom.h

---
+++ module/src/main/cpp/Custom.h
@@ -0,0 +1,36 @@
+void styl() {
+	ImGuiStyle& style = ImGui::GetStyle();
+
+    style.Colors[ImGuiCol_Text] = ImColor(255, 255, 255);
+    style.Colors[ImGuiCol_WindowBg] = ImColor(0, 2, 13);
+    style.Colors[ImGuiCol_TitleBg] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_TitleBgActive] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_Button] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ButtonActive] = ImColor(130, 0, 177);
+    style.Colors[ImGuiCol_ButtonHovered] = ImColor(130, 0, 177);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ScrollbarGrab] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImColor(111, 123, 247);
+    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImColor(130,0,177);
+    style.Colors[ImGuiCol_ResizeGripActive] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_ResizeGrip] = ImColor(13, 118, 220);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_TitleBgActive] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_CheckMark] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_SliderGrab] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_HeaderActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_HeaderHovered] = ImColor(114, 2, 149);
+    style.Colors[ImGuiCol_Header] = ImColor(114, 2, 149);
+	style.Colors[ImGuiCol_FrameBg] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_FrameBgActive] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_Button] = ImColor(130,0,177);
+	style.Colors[ImGuiCol_ButtonHovered] = ImColor(150,0,205);
+	style.Colors[ImGuiCol_ButtonActive] = ImColor(150,0,205);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImColor(0, 2, 13);
+
+	style.Colors[ImGuiCol_PopupBg] = ImColor(0, 2, 13);
+	
+
+    }
