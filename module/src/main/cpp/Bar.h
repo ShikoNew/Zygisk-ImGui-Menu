be55ffbc81860ce3b83b4b0fe85bdff8d417e5bf
Ueu01929 <134735875+Ueu01929@users.noreply.github.com>
2024-01-09 23:48:31 -0600

    Update Bar.h

--- module/src/main/cpp/Bar.h
+++ module/src/main/cpp/Bar.h
@@ -1,5 +1,5 @@
 static int m16rn = 0;
 const char* m16type[] = { "Disable", "Blue","Red"};
 static int sizer = 0;
-const char* sizetype[] = { "Disable", "Blue","Blue fantasy", "XD?", "Winged", "Needle", "Muraena", "Facet", "Camouflage", "Retro Arcade", "Sharps Shooter"};
+const char* sizetype[] = { "Disable", "Purple"};
 
